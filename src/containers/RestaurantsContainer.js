49fe2705fd06016dc2cb78ef1f0d7e3ee57be61b
jrog20 <jaimerogstad@gmail.com>
2021-07-19 15:19:42 -0400

    commit tree 1369988a1d8fb0befff492f09700c09ab9b008f7

--- src/containers/RestaurantsContainer.js
+++ src/containers/RestaurantsContainer.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
-import RestaurantInput from '../components/restaurants/RestaurantInput';
-import Restaurants from '../components/restaurants/Restaurants';
+import RestaurantInput from '../components/restaurants/RestaurantInput'
+import Restaurants from '../components/restaurants/Restaurants'
 import { connect } from 'react-redux'
 
 class RestaurantsContainer extends Component {
-
   render() {
     return (
       <div>
-        <RestaurantInput addRestaurant={this.props.addRestaurant} />
-        <Restaurants restaurants={this.props.restaurants} deleteRestaurants={this.props.deleteRestaurants} />
+        <RestaurantInput addRestaurant={this.props.addRestaurant}/>
+        <Restaurants
+          restaurants={this.props.restaurants}
+          deleteRestaurant={this.props.deleteRestaurant}
+        />
       </div>
-    )
+    );
   }
 }
 
@@ -22,4 +24,4 @@ const mapDispatchToProps = dispatch => ({
   deleteRestaurant: id => dispatch({type: 'DELETE_RESTAURANT', id})
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(RestaurantsContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(RestaurantsContainer)
