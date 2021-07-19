af1a661c338ea4f2aa89c66e011e326b6ffb1c2c
jrog20 <jaimerogstad@gmail.com>
2021-07-18 20:07:28 -0400

    Rework form

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+
+  // constructor(props) {
+  //   super(props);
+
+    state = {
+      name: ''
+    };
+  // };
+
+  handleOnChange = event => {
+    this.setState ({
+      name: event.target.value
+    })
+  }
+
+  handleOnSubmit = event => {
+    event.preventDefault();
+
+  }
+
   render() {
     return (
       <div>
-        Restaurant Input
+        Enter New Restaurant:
+        <form onSubmit={this.handleOnSubmit}>
+          <input type='text' name='name' value={this.state.name} onChange={this.handleOnChange}>Restaurant Name:</input>
+          <input type='submit' />  
+        </form>      
       </div>
     );
   }
